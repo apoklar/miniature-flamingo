f859fe56888e32d1853d0b41b79279b995aa2ad9
andrp <a.poklar@yahoo.com>
2021-05-01 21:41:06 -0500

    added code to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,20 @@ const fs = require('fs');
 
 module.exports = app => {
     app.get('/api/notes', (req, res) => {
-
+        let results = notes;
+        if (req.query) {
+          results = filterByQuery(req.query, results);
+        }
+        res.json(results);
     })
+    
     app.post('/api/notes', (req, res) => {
-        
+        req.body.id = notes.length.toString();
+        res.json(notes);
     })
+    
     app.delete('/api/notes/:id', (req, res) => {
-        
+        deleteNote(notes, req.params.id);
+        res.json(notes);
     })
-}
+};
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
+
 app.use(express.json());
 app.use(express.static('public'));
 
