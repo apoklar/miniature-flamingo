54f4c524a15cad53514b9db3fbcf0008442e97f7
andrp <a.poklar@yahoo.com>
2021-05-06 12:49:30 -0500

    heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,38 @@
 const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = app => {
     app.get('/api/notes', (req, res) => {
-        let results = notes;
-        if (req.query) {
-          results = filterByQuery(req.query, results);
-        }
-        res.json(results);
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+            if (err) throw err 
+            res.json(JSON.parse(data)) 
+        })
     })
     
     app.post('/api/notes', (req, res) => {
-        req.body.id = notes.length.toString();
-        res.json(notes);
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+            if (err) throw err 
+            req.body.id = uuidv4()
+            const oldNotes = (JSON.parse(data)) 
+            const newNotes = [req.body, ...oldNotes]
+            
+            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(newNotes), err =>{
+                if (err) throw err
+                res.json(true)
+            })
+        })
     })
     
     app.delete('/api/notes/:id', (req, res) => {
-        deleteNote(notes, req.params.id);
-        res.json(notes);
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+            if (err) throw err 
+            const oldNotes = (JSON.parse(data))
+            const newNotes = oldNotes.filter(note => note.id != req.params.id)
+
+            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(newNotes), err =>{
+                if (err) throw err
+                res.json(true)
+            })
     })
-}; 
+})};
